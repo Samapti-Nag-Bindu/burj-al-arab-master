9bc5abb8cac37dbf30925f340a5787277f6c7030
Samapti Nag Bindu <jagadbandhu22@gmail.com>
2021-03-18 02:46:43 -0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBsDryLFWrrF_DW0xvITSPvISE0I4ZlAHM",
+  authDomain: "burj-al-arab-c8021.firebaseapp.com",
+  projectId: "burj-al-arab-c8021",
+  storageBucket: "burj-al-arab-c8021.appspot.com",
+  messagingSenderId: "1053631817380",
+  appId: "1:1053631817380:web:aa11eea5d3d7021dbce831"
+};
+
+export default firebaseConfig;
